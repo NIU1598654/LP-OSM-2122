b151798a23d0cf3a05f73634199d71cc403196a2
NIU1598654 <73404147+NIU1598654@users.noreply.github.com>
2021-10-17 23:46:59 +0200

    Actualizacion Botiga

--- LP-OSM-2122-DLL/PuntDeInteresBotigaSolucio.h
+++ LP-OSM-2122-DLL/PuntDeInteresBotigaSolucio.h
@@ -1,15 +1,29 @@
 #pragma once
-
 #include "PuntDeInteresBase.h"
 
 class PuntDeInteresBotigaSolucio : public PuntDeInteresBase
 {
+
+private:
+	string m_nombre;
+
 public:
-	PuntDeInteresBotigaSolucio() {}
-	~PuntDeInteresBotigaSolucio() {}
+	PuntDeInteresBotigaSolucio() { m_nombre = ""; };
+	~PuntDeInteresBotigaSolucio() {};
 
 	string getName() { return m_nombre; }
-private:
-	string m_shop, m_nombre;
+
+	unsigned int getColor()
+	{
+		if (m_nombre == "supermarket")		//No usamos un swtch case porque no acepta un argumento de tipo string, solo de tipo numerico o char.
+			return 0xA5BE00;
+		else if (m_nombre == "tobacco")
+			return 0XFFAD69;
+		else if (m_nombre == "bakery")
+			return 0XE85D75;
+		else
+			return 0xEFD6AC;
+	}
+
 
 };
