b7835ead9fb85f59d6bf5bdb9203b73113446d8f
Martí Bruix Fernández <Marti.Bruix@e-msoffice.uab.cat>
2021-10-17 21:46:34 +0200

    tasca4

---
+++ LP-OSM-2122-DLL/CamiSolucio.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CamiSolucio.h"
+
+std::vector<Coordinate> CamiSolucio::getCamiCoords()
+{
+	std::vector<Coordinate> v;
+	Coordinate c;
+	c.lat = 41.4928803;
+	c.lon = 2.1452381;
+	v.push_back(c);
+	c.lat = 41.4929072;
+	c.lon = 2.1452474;
+	v.push_back(c);
+	c.lat = 41.4933070;
+	c.lon = 2.1453852;
+	v.push_back(c);
+	c.lat = 41.4939882;
+	c.lon = 2.1456419;
+	v.push_back(c);
+	return v;
+}
